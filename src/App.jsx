b12dc36f0722693ed3f3f130c36a88d46e1e3727
marjanmokhtari <mimmokhtarii@gmail.com>
2025-07-26 01:06:19 +0330

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import CoinLogo from './assets/coinmarketcap_white_1.svg'
+import Header from './components/header/header'
+import Main from './components/main/main'
 
 function App() {
   const [coin, setCoin] = useState(null)
 
 
   return (
-   <div>
-    <img src={CoinLogo} alt="" />
-   </div>
+    <div className='w-full flex justify-center'>
+      <div className='w-full 2xl:container text-amber-50 '>
+       <Header/>
+       <Main/>
+      </div>
+      {/* <img src={CoinLogo} alt="" /> */}
+    </div>
   )
 }
 
