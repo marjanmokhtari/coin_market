44ff580d8626d6a3679dce3b801c4b0bf4e55423
marjanmokhtari <mimmokhtarii@gmail.com>
2025-07-27 00:01:53 +0330

    add table

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from './components/header/header'
 import Main from './components/main/main'
 
+
 function App() {
-  const [coin, setCoin] = useState(null)
+  const [coin, setCoin] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+   useEffect(() => {
+    fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true'
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setCoin(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
 
+  console.log(coin);
+  
+  
   return (
     
     <div className='w-full flex justify-center'>
       <div className='w-full 2xl:container text-amber-50 p-4'>
        <Header/>
-       <Main/>
+       <Main coin={coin}/>
       </div>
       
     </div>
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
-export default function main() {
+export default function main({ coin }) {
+
+
   return (
-    <div>main</div>
+    <>
+      {/* <h1>fsrfvsdfz</h1>
+      <div className='border'>
+        {coin && coin.map((coin) => {
+          return (
+            <ul key={coin.id}>
+              <li>{coin.name}</li>
+              <li>{coin.symbol.toUpperCase()}</li>
+              <li> ${coin.current_price.toLocaleString('en-US', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}</li>
+              <li>{coin.price_change_percentage_24h?.toFixed(2)}%</li>
+              <li>${coin.market_cap.toLocaleString('en-US')}</li>
+            </ul>
+          )
+        })}
+      </div> */}
+
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead>
+          <tr>
+            <th className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">#</th>
+            <th className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+            <th className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price</th>
+            <th className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">24h %</th>
+            <th className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Market Cap</th>
+            <th className="px-3 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Last 7 Days</th>
+          </tr>
+        </thead>
+        <tbody className=" divide-y divide-gray-200">
+          {coin.map((coin, i) => (
+            <tr key={coin.id} className="hover:bg-hoverblue">
+              <td>{i + 1}</td>
+              <td className="px-3 py-4 whitespace-nowrap text-sm font-medium ">{coin.name}</td>
+              <td className="px-3 py-4 whitespace-nowrap text-sm ">
+                ${coin.current_price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+              </td>
+              <td
+                className={`px-6 py-4 whitespace-nowrap text-sm font-semibold ${coin.price_change_percentage_24h < 0 ? 'text-red-600' : 'text-green-600'
+                  }`}
+              >
+                {coin.price_change_percentage_24h?.toFixed(2)}%
+              </td>
+              {/* <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                ${coin.total_volume.toLocaleString('en-US')}
+              </td> */}
+              <td className="px-3 py-4 whitespace-nowrap text-sm text-gray-500">
+                ${coin.market_cap.toLocaleString('en-US')}
+              </td>
+              <td className="px-3 py-4 whitespace-nowrap ">
+                {coin.sparkline_in_7d?.price ? (
+                  <Sparklines data={coin.sparkline_in_7d.price} width={200} height={20}>
+                    <SparklinesLine color={coin.price_change_percentage_24h < 0 ? "red" : "green"} />
+                  </Sparklines>
+                ) : (
+                  <span>No data</span>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   )
 }
