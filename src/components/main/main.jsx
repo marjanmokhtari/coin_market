4d638f3e6b83fe9dbff6ad85bbaee2a629c8cdb0
marjanmokhtari <mimmokhtarii@gmail.com>
2025-07-28 12:54:35 +0330

    edit table

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -12,31 +12,39 @@ export default function main({ coin }) {
             <th className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
             <th className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price</th>
             <th className="px-3 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">24h %</th>
-            <th className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Market Cap</th>
-            <th className="px-3 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Last 7 Days</th>
+            <th className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden xl:block">Market Cap</th>
+            <th className="px-3 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider ">Last 7 Days</th>
           </tr>
         </thead>
         <tbody className=" divide-y divide-line">
           {coin.map((coin, i) => (
             <tr key={coin.id} className="hover:bg-hoverblue">
-              <td className='text-[14px] font-medium text-center'>{i + 1}</td>
+              <td className='text-xs xl:text-[14px] font-medium text-center'>{i + 1}</td>
               <td className="px-3 py-4 whitespace-nowrap   flex gap-2 items-center">
                 <img className='w-6' src={coin.image} alt="" />
-                <span className='text-[14px] font-medium'>{coin.name}</span>
+                {/* نمایش روی موبایل و تبلت */}
+                <span className="text-xs font-medium inline xl:hidden">
+                  {coin.symbol.toUpperCase()}
+                </span>
+
+                {/* نمایش روی دسکتاپ */}
+                <span className="text-[14px] font-medium hidden xl:inline ">
+                  {coin.name}
+                </span>
               </td>
-              <td className="px-3 py-4 whitespace-nowrap text-sm ">
+              <td className="px-3 py-4 whitespace-nowrap text-xs xl:text-sm ">
                 ${coin.current_price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
               </td>
               <td
-                className={`px-6 py-4 whitespace-nowrap text-sm font-semibold text-center  ${coin.price_change_percentage_24h < 0 ? 'text-red-600' : 'text-green-600'
+                className={`px-6 py-4 whitespace-nowrap text-xs xl:text-sm font-semibold text-center  ${coin.price_change_percentage_24h < 0 ? 'text-red-600' : 'text-green-600'
                   }`}
               >
                 {coin.price_change_percentage_24h?.toFixed(2)}%
               </td>
-              <td className="px-3 py-4 whitespace-nowrap text-sm text-gray-500">
+              <td className="px-3 py-4 whitespace-nowrap text-sm text-gray-500 hidden xl:flex">
                 ${coin.market_cap.toLocaleString('en-US')}
               </td>
-              <td className="px-3 py-4 whitespace-nowrap ">
+              <td className="px-3 py-4 whitespace-nowrap   ">
                 {coin.sparkline_in_7d?.price ? (
                   <Sparklines data={coin.sparkline_in_7d.price} width={200} height={20}>
                     <SparklinesLine color={coin.price_change_percentage_24h < 0 ? "red" : "green"} />
